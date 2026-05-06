d70ca3a61410572478cfcb9213b48cab8bf3d670
frostyragehd <frostyragehd@gmail.com>
2026-05-06 04:56:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const { serveHTTP } = require("stremio-addon-sdk");
 const addonInterface = require("./addon");
 
 addonInterface.then(resolvedInterface => {
-    serveHTTP(resolvedInterface, { port: process.env.PORT || 7000 });
+    // Port 10000 is default for Render
+    serveHTTP(resolvedInterface, { port: process.env.PORT || 10000 });
 }).catch(err => {
     console.error("Failed to start:", err);
 });
