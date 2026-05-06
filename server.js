46e80c0a061de5475618574f727bef4b1acb260e
frostyragehd <frostyragehd@gmail.com>
2026-05-06 04:53:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const { serveHTTP } = require("stremio-addon-sdk");
 const addonInterface = require("./addon");
 
-// Render uses the PORT environment variable
-serveHTTP(addonInterface, { port: process.env.PORT || 7000 });
+addonInterface.then(resolvedInterface => {
+    serveHTTP(resolvedInterface, { port: process.env.PORT || 7000 });
+}).catch(err => {
+    console.error("Failed to start:", err);
+});
